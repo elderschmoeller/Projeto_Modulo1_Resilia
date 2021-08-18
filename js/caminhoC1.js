6c0d8d6cf3959bfa8c30a77a4651f8c434f9425f
Elder Schmoeller <elderschmoellerls@gmail.com>
2021-08-18 12:41:58 -0300

    Finalização Projeto RPG Módulo 1 Resilia

--- js/caminhoC1.js
+++ js/caminhoC1.js
@@ -1,7 +1,7 @@
 function caminhoC1() {
     // caminhoTresA() 
 
-var pathSelect = prompt('Select your path. : \n 1 - You facing an group of smaller orcs, do you prefer catch them stealth ?  \n 2 - You facing an group of smaller orcs, do you have to have a really good aim-shot to defeat them like that \n 3 - ')
+var pathSelect = prompt('Select your path. : \n 1 - You facing an group of smaller orcs, do you prefer catch them stealth ?  \n 2 - You facing an group of smaller orcs, do you have to have a really good aim-shot to defeat them like that \n 3 - Or would you prefer the Orcs to leave the location and you have no problems ?')
 
 if (pathSelect == 1) {
     caminhoB1()
